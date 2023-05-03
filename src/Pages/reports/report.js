aff70a1a2fa5791fd9bd29ebc4b91a5089851163
tushar harde <vijay@zimbaroosgeeks.in>
2023-05-03 23:13:45 +0530

    Validation added to the reports page

--- src/Pages/reports/report.js
+++ src/Pages/reports/report.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./report.css";
 import MiniDrawer from "../../components/sideMenu";
 
-import { useTheme } from '@mui/material/styles';
-import { Card, CardHeader, CardContent, Typography, MenuItem } from '@material-ui/core';
+import { Card, CardContent, MenuItem } from '@material-ui/core';
 import SummarizeOutlinedIcon from '@mui/icons-material/SummarizeOutlined';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
@@ -14,16 +13,75 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Button from '@mui/material/Button';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    disabledButton: {
+      background: "#FAC3B3 !important",
+    },
+
+    enableButton: {
+        background: "#F26841 !important",
+    }
+  });
 
 const Report = () => {
 
-    const theme = useTheme();
+    const reports = {
+        "responseData": {
+            "EER": [{
+                "reportId": "644a01cf85d675688b9df9dc",
+                "reportCode": "EER",
+                "reportName": "EXCESS_EXCEPTION_REPORT",
+                "isActive": "Y",
+                "createdOn": 1682577658136,
+                "createdBy": "Rahul Verma",
+                "updatedOn": 1682577658136,
+                "updatedBy": "Rahul Verma"
+            },
+            {
+                "reportId": "644a01cf85d675cxcfxv688b9df9dc",
+                "reportCode": "EER",
+                "reportName": "EXCESS_EXCEPTION",
+                "isActive": "Y",
+                "createdOn": 1682577658136,
+                "createdBy": "Rahul Verma",
+                "updatedOn": 1682577658136,
+                "updatedBy": "Rahul Verma"
+            }
+            ]
+        },
+        "message": "Successful",
+        "error": false,
+        "statusCode": "OK"
+    }
 
-    const [age, setAge] = React.useState('');
+    const [report, setReport] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('')
+
+    const classes = useStyles();
 
     const handleChange = (event) => {
-        setAge(event.target.value);
+        setReport(event.target.value);
     };
+
+    const generateReport = () => {
+        console.log("startDate", startDate.$d);
+        console.log("End Date",endDate.$d);
+        const data = reports.responseData.EER.filter(ele => ele.reportId == report)[0];
+        console.log("Data------",data);
+    }
+
+    const resetValues = () => {
+        setReport('');
+        setStartDate('');
+        setEndDate('');
+    }
+    
+    const isFormValid = () => {
+        return (report !== "" &&  startDate !== "" && endDate !== "");
+    }
 
     return (
         <div>
@@ -31,16 +89,26 @@ const Report = () => {
             <MiniDrawer />
             {/* <Toolbar /> */}
             <div className="shadow" style={{ marginLeft: '65px', paddingLeft: '10px' }}>
-                <div style={{display:'flex',justifyContent:'space-between'}}>
-                <h3 style={{ display: "flex",margin:'0',paddingTop:'10px' }}>
-                    <div style={{ marginRight: "10px" }}><ArrowBackIcon /></div>
-                    Basic Details Of Report. 
-                </h3>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <h3 style={{ display: "flex", margin: '0', paddingTop: '10px' }}>
+                        <div style={{ marginRight: "10px" }}><ArrowBackIcon /></div>
+                        Basic Details Of Report.
+                    </h3>
 
-                <div style={{paddingTop:'10px'}}>
-                    <Button variant="outlined">Reset</Button>
-                    <Button variant="contained" style={{margin:"5px", background:'#F26841'}}>Generate Report</Button>
-                </div>
+                    <div style={{ paddingTop: '10px' }}>
+                        <Button 
+                        variant="outlined"
+                        onClick={resetValues}
+                        >Reset</Button>
+                        <Button 
+                        variant="contained" 
+                        disabled={!isFormValid()}
+                        className={!isFormValid() ? classes.disabledButton : classes.enableButton}
+                        onClick={generateReport}
+                        style={{ margin: "5px"}}
+                        >Generate Report
+                        </Button>
+                    </div>
 
                 </div>
             </div>
@@ -63,21 +131,27 @@ const Report = () => {
                                         <Select
                                             labelId="demo-simple-select-standard-label"
                                             id="demo-simple-select-standard"
-                                            value={age}
+                                            value={report}
                                             onChange={handleChange}
-                                            label="Age"
+                                            label="Report"
+                                            style={{ width: '300px' }}
                                         >
-                                            <MenuItem value="">
+                                            {reports.responseData.EER.map((report) => (
+                                                <MenuItem key={report.reportId} value={report.reportId}>
+                                                    {report.reportName}
+                                                </MenuItem>
+                                            ))}
+                                            {/* <MenuItem value="">
                                                 <em>None</em>
                                             </MenuItem>
                                             <MenuItem value={10}>One</MenuItem>
                                             <MenuItem value={20}>Two</MenuItem>
-                                            <MenuItem value={30}>Three</MenuItem>
+                                            <MenuItem value={30}>Three</MenuItem> */}
                                         </Select>
                                     </FormControl>
                                 </div>
 
-                                <div>
+                                {/* <div>
                                     <h4 style={{ color: "#365069", margin: "0" }}>Version</h4>
 
                                     <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
@@ -97,13 +171,15 @@ const Report = () => {
                                             <MenuItem value={30}>Three</MenuItem>
                                         </Select>
                                     </FormControl>
-                                </div>
+                                </div> */}
                             </div>
                         </CardContent>
                     </Card>
                 </div>
 
                 <div style={{ width: "65%", marginTop: '25px' }}>
+
+                    {report.length > 0 &&
                     <Card>
                         <div style={{ paddingLeft: "40px", color: "#F8886D", borderBottom: "1px solid gray" }}>
                             <h3 style={{ display: "flex" }}>
@@ -116,19 +192,27 @@ const Report = () => {
                                 <div>
                                     <h4 style={{ color: "#365069", }}>Start Date</h4>
                                     <LocalizationProvider dateAdapter={AdapterDayjs} style={{ marginTop: '10px' }}>
-                                        <DatePicker />
+                                        <DatePicker
+                                            value={startDate}
+                                            onChange={(date) => setStartDate(date)}
+                                        />
                                     </LocalizationProvider>
                                 </div>
 
                                 <div>
                                     <h4 style={{ color: "#365069" }}>End Date</h4>
                                     <LocalizationProvider dateAdapter={AdapterDayjs} style={{ marginTop: '10px' }}>
-                                        <DatePicker />
+                                        <DatePicker
+                                              value={endDate}
+                                              onChange={(date) => setEndDate(date)}
+                                        />
                                     </LocalizationProvider>
                                 </div>
                             </div>
                         </CardContent>
                     </Card>
+                    }
+
                 </div>
             </div>
         </div>
